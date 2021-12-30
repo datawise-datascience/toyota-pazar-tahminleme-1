b51b4baeceed33479c6addaa3ec670a51ad0f32f
gorkemdatawise <94366939+datawise-datascience@users.noreply.github.com>
2021-12-30 10:17:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-import lightgbm as lgb
 import pickle
 import time
 import datetime as dt
